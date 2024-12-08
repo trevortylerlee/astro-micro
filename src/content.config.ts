ccf78154d711a40a0f124e7d40b89149e5bc34ab
AK <89195061+anaxite@users.noreply.github.com>
2024-12-08 18:29:38 +0100

    Change content collection pattern
    - Change mdx file extension pattern per official docs
    - Remove underscore

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from "astro:content";
 import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.(md|mdx)', base: "./src/content/blog" }),
+  loader: glob({ pattern: '**/*.{md,mdx}', base: "./src/content/blog" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -13,7 +13,7 @@ const blog = defineCollection({
 });
 
 const projects = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.(md|mdx)', base: "./src/content/projects" }),
+  loader: glob({ pattern: '**/*.{md,mdx}', base: "./src/content/projects" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
