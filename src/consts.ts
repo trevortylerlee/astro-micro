a7f1cf399b8596be1eb3df758e95f585864893a0
Trevor Lee <49603972+trevortylerlee@users.noreply.github.com>
2025-01-24 23:01:20 -0800

    update socials

--- src/consts.ts
+++ src/consts.ts
@@ -27,14 +27,14 @@ export const PROJECTS: Metadata = {
 export const SOCIALS: Socials = [
   {
     NAME: "X (formerly Twitter)",
-    HREF: "https://twitter.com/boogerbuttcheeks",
+    HREF: "https://twitter.com/boogerbuttcheek",
   },
   {
     NAME: "GitHub",
     HREF: "https://github.com/trevortylerlee",
   },
   {
-    NAME: "LinkedIn",
-    HREF: "https://www.linkedin.com/in/trevortylerlee",
+    NAME: "Website",
+    HREF: "https://trevortylerlee.com",
   },
 ];
